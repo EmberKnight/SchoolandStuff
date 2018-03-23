2963e444e6f696130db951070baed5c99c2fb37a
EmberKnight <dennis.stancov@outlook.com>
2018-03-23 17:03:27 +0100

    Test

--- src/Figuren.java
+++ src/Figuren.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Figuren {
 
-    public void rechteck(int hoehe, int breite) {
+    public void Rechteck(int hoehe, int breite) {
 
         int i, j, stern, leer;
         leer = 0;
@@ -24,6 +26,30 @@ public class Figuren {
 
         }
 
+
+    }
+
+    public void aT(int hoehe) {
+
+        int leer, i, j, a;
+        leer = 1;
+
+        for (i = 0; i < hoehe; i++) {
+
+            for (j = 0; j < hoehe - leer; j++) {
+
+                System.out.print(" ");
+
+            }
+
+            for (a = 0; a < leer; a++) {
+
+                System.out.print("@");
+
+            }
+            System.out.println();
+            leer = leer + 1;
+        }
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        int y = scan.nextInt();
+             Figuren test = new Figuren();
+             test.aT(6);
 
-        Figuren test = new Figuren();
-        test.rechteck(x, y);
+        }
 
-        IfElse test1 = new IfElse();
-        test1.Score();
-
-        Switch test2 = new Switch();
-        test2.Case();
-
-    }
 }

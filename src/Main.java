1fa7ed08ad2cf5aa9564c7719292ac7fac83dc71
EmberKnight <dennis.stancov@outlook.com>
2018-03-23 15:38:31 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+
         Figuren test = new Figuren();
-        test.rechteck(5,10);
+        test.rechteck(x, y);
 
     }
 }

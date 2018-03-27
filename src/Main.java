9189ec6695a0b4253eacb1a0d3d47147c16cdcd5
EmberKnight <dennis.stancov@outlook.com>
2018-03-27 23:17:28 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-             Figuren test = new Figuren();
-             test.aT(6);
-
-             Switch test1 = new Switch();
-             test1.Case();
+            WhileLoop test = new WhileLoop();
+            test.Count();
 
         }
 
---
+++ src/WhileLoop.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+
+
+    public void Count() {
+
+        int anzahlGegner = 0;
+
+        Scanner scan = new Scanner(System.in);
+        int userInput = scan.nextInt();
+        System.out.println("Choose difficulty!");
+
+        if (userInput == 1) {
+
+            anzahlGegner = 10;
+
+        }
+        else if (userInput == 2) {
+
+            anzahlGegner = 15;
+
+        }
+
+        while(anzahlGegner != 0) {
+            System.out.println("Gegner erstellt!");
+            anzahlGegner--;
+        }
+
+    }
+
+}

1d3d6b3091ed3bd2bddad8eef87beefa92fd6c4b
EmberKnight <dennis.stancov@outlook.com>
2018-03-29 21:40:17 +0200

    Test

---
+++ src/Laptop.java
@@ -0,0 +1,39 @@
+public class Laptop {
+
+    int ram;
+    int preis;
+    String marke;
+
+    public Laptop(int ram, int preis, String marke) {
+        setRam(ram);
+        setPreis(preis);
+        setMarke(marke);
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void setPreis(int preis) {
+        if (preis < 2000 && preis > 500) {
+            this.preis = preis;
+        } else {
+            System.out.println("Der Preis stimmt nicht!");
+        }
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public String toString() {
+
+        return "Marke: " + marke + "\nRam: " + ram + "\nPreis: " + preis;
+
+    }
+
+    public void printToString() {
+        System.out.println(toString());
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,18 @@ public class Main {
         test.Fullen();
         test.minPos(6,5); */
 
-        Auto bmw = new Auto(4, 5000, "BMW");
+       /* Auto bmw = new Auto(4, 5000, "BMW");
         System.out.println(bmw);
 
         System.out.println("Preis:  " + bmw.preis + " Reifen: "  + bmw.reifen + " Marke: " + bmw.marke);
 
         Auto audi = new Auto(4, 7000, "Audi");
         audi.comparePrice(10000);
-        bmw.comparePrice(3000);
+        bmw.comparePrice(3000); */
+
+        Laptop lenovo = new Laptop(8, 1000, "Lenovo");
+        lenovo.printToString();
+
 
     }
 
